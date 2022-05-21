378891634cbbf9ba6ae178d9281f2c3fcc843b6a
Matthieu LEMOINE <8517072+MatthieuLemoine@users.noreply.github.com>
2022-05-21 12:35:38 +0200

    feat: support array of enum

--- prisma/sequelize/models/Charity.ts
+++ prisma/sequelize/models/Charity.ts
@@ -14,6 +14,30 @@ export class Charity extends Model {
           type: DataTypes.STRING,
           allowNull: false,
         },
+        sustainableDevelopmentGoals: {
+          type: DataTypes.ARRAY(
+            DataTypes.ENUM(
+              'NO_POVERTY',
+              'ZERO_HUNGER',
+              'GOOD_HEALTH_AND_WELL_BEING',
+              'QUALITY_EDUCATION',
+              'GENDER_EQUALITY',
+              'CLEAN_WATER_AND_SANITATION',
+              'AFFORDABLE_AND_CLEAN_ENERGY',
+              'DECENT_WORK_AND_ECONOMIC_GROWTH',
+              'INDUSTRY_INNOVATION_AND_INFRASTRUCTURE',
+              'REDUCED_INEQUALITIES',
+              'SUSTAINABLE_CITIES_AND_COMMUNITIES',
+              'RESPONSIBLE_CONSUMPTION_AND_PRODUCTION',
+              'CLIMATE_ACTION',
+              'LIFE_BELOW_WATER',
+              'LIFE_ON_LAND',
+              'PEACE_JUSTICE_AND_STRONG_INSTITUTIONS',
+              'PARTNERSHIPS_FOR_THE_GOALS'
+            )
+          ),
+          allowNull: false,
+        },
       },
       {
         sequelize,
